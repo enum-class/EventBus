8cbfb48d1338838a7f1ca9f44139e4f933114306
enum-class <samaan.dehghan@gmail.com>
2023-02-04 20:23:52 +0800

    Fix format

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -2,5 +2,5 @@
 
 for run in {1..2}
 do
-  git clang-format HEAD^ -- include/*.h src/*.cpp tests/*.cpp superqueue/*.h -f
+  git clang-format HEAD^ -- include/*.h src/*.cpp tests/*.cpp superqueue/*.h superfactory/*.h -f
 done
--- superfactory/SuperFactory.h
+++ superfactory/SuperFactory.h
@@ -1,64 +1,59 @@
 #include <SuperQueue.h>
 
-namespace
-{
+namespace {
 
 struct SuperMemPool {
-    uint8_t *data = nullptr;
-    superqueue::SuperQueue *pool = nullptr;
+  uint8_t *data = nullptr;
+  superqueue::SuperQueue *pool = nullptr;
 };
 
 static inline SuperMemPool *create_pool(std::size_t size, std::size_t block) {
-    SuperMemPool *mempool = new SuperMemPool;
-    mempool->pool = superqueue::create(size);
-    mempool->data = new uint8_t[size * block];
+  SuperMemPool *mempool = new SuperMemPool;
+  mempool->pool = superqueue::create(size);
+  mempool->data = new uint8_t[size * block];
 
-    for (int i = 0; i < size; i++) {
-        superqueue::enqueue<superqueue::SyncType::SINGLE_THREAD,
-            superqueue::Behavior::FIXED>(mempool->pool, &mempool->data[i * block]);
-    }
+  for (int i = 0; i < size; i++) {
+    superqueue::enqueue<superqueue::SyncType::SINGLE_THREAD,
+                        superqueue::Behavior::FIXED>(mempool->pool,
+                                                     &mempool->data[i * block]);
+  }
 
-    return mempool;
+  return mempool;
 }
 
 static inline uint8_t *acquire(SuperMemPool *mempool) noexcept {
-    return (uint8_t*)superqueue::dequeue<superqueue::SyncType::MULTI_THREAD,
-        superqueue::Behavior::FIXED>(mempool->pool, 1);
+  return (uint8_t *)superqueue::dequeue<superqueue::SyncType::MULTI_THREAD,
+                                        superqueue::Behavior::FIXED>(
+      mempool->pool, 1);
 }
 
-static inline void release(SuperMemPool *mempool, uint8_t* buffer) noexcept {
-    superqueue::enqueue<superqueue::SyncType::SINGLE_THREAD,
-        superqueue::Behavior::FIXED>(mempool->pool, buffer);
+static inline void release(SuperMemPool *mempool, uint8_t *buffer) noexcept {
+  superqueue::enqueue<superqueue::SyncType::SINGLE_THREAD,
+                      superqueue::Behavior::FIXED>(mempool->pool, buffer);
 }
 
-}
+} // namespace
 
-namespace superfactory
-{
+namespace superfactory {
 
-template<std::size_t SIZE, std::size_t BLOCK>
-class SuperFactory {
-    SuperMemPool *mempool = nullptr;
+template <std::size_t SIZE, std::size_t BLOCK> class SuperFactory {
+  SuperMemPool *mempool = nullptr;
+
 public:
-    SuperFactory()
-    : mempool(create_pool(SIZE, BLOCK))
-    {
-    }
+  SuperFactory() : mempool(create_pool(SIZE, BLOCK)) {}
 
-    template <class TEvent, class... Args>
-    TEvent *create(Args... args) {
-        uint8_t *buf = acquire(mempool);
-        if (buf)
-            return new (buf) TEvent(std::forward<Args>(args)...);
+  template <class TEvent, class... Args> TEvent *create(Args... args) {
+    uint8_t *buf = acquire(mempool);
+    if (buf)
+      return new (buf) TEvent(std::forward<Args>(args)...);
 
-        return nullptr;
-    }
+    return nullptr;
+  }
 
-    template <class TEvent>
-    void recycle(TEvent *event) {
-        event->~TEvent();
-        release(mempool, reinterpret_cast<uint8_t *>(event));
-    }
+  template <class TEvent> void recycle(TEvent *event) {
+    event->~TEvent();
+    release(mempool, reinterpret_cast<uint8_t *>(event));
+  }
 };
 
-}
+} // namespace superfactory
